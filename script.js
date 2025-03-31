a49bc945709a7ecaae0858e6961bb17788d913f2
Kanchan <kanchan96437@gmail.com>
2025-03-31 21:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ name: "John",
 
 console.log(getKeys(student)); 
 }
+const student2 = {
+  name: "Jane",
+  age: 22,
+  city: "New York"
+};
+
+console.log(getKeys(student2)); 
+
+const student3 = {
+  subject: "Math"
+};
+
+console.log(getKeys(student3));
